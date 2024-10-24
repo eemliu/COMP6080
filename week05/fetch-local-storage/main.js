3ce53afba91ed619e6b1a1db42b10d038071ae4c
Emily Liu <emilyliu496@gmail.com>
2024-10-24 14:38:16 +1100

    change week 7 demo

--- week05/fetch-local-storage/main.js
+++ week05/fetch-local-storage/main.js
@@ -1,30 +1,31 @@
-const output = document.getElementById("output");
-
+const output = document.getElementById('output');
 
 const storeData = () => {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
 
   const userData = {
     name: name,
-    email: email,
-  };
+    email: email
+  }
 
-  const userDataJSON = JSON.stringify(userData);
-  localStorage.setItem("userData", userDataJSON);
-  output.textContent = "Data stored!";
+  const parsedUserData = JSON.stringify(userData);
+  localStorage.setItem('userData', parsedUserData);
+
+  output.innerText = 'Data stored!'
 }
 
 const retrieveData = () => {
-  const userDataJSON = localStorage.getItem("userData");
-  const userData = JSON.parse(userDataJSON);
+  const parsedUserData = localStorage.getItem('userData');
+  const userData = JSON.parse(parsedUserData);
 
   if (!userData) {
-    output.textContent = "No data found!";
-    return
+    output.textContent = 'No data found!';
+    return;
   } else {
     const name = userData.name;
     const email = userData.email;
     output.textContent = `Retrieved data: Name: ${name}, Email: ${email}`;
   }
 }
+
